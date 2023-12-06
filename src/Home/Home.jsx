7139e534a2fc57cf1712958213b1204e6e56fa1e
quangman <quangmank16@gmail.com>
2023-12-06 18:47:20 +0700

    merge

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "../Header/Header";
 import Carousel from "../Carousel/Carousel";
+import Application from "../Application/Application";
+import Footer from "../Footer/Footer";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Carousel />
+      <Application />
+      <Footer />
     </div>
   );
 }
