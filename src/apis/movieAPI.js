f99f56f67910dd84d18f9dfb23ef84dd84bc9dbe
TrungTran <trungtran470@gmail.com>
2023-12-30 17:08:46 +0700

    update movie

--- src/apis/movieAPI.js
+++ src/apis/movieAPI.js
@@ -13,7 +13,7 @@ export async function getMoviesAPI() {
   try {
     const resp = await baseAPI.get("/QuanLyPhim/LayDanhSachPhim", {
       params: {
-        maNhom: "GP01",
+        maNhom: "GP02",
       },
     });
 
@@ -28,7 +28,7 @@ export async function createMovieAPI(movie) {
     for (let key in movie) {
       formData.append(key, movie[key]);
     }
-    formData.append("maNhom", "GP01");
+    formData.append("maNhom", "GP02");
     const resp = await baseAPI.post("/quanlyphim/themphimuploadhinh", formData);
     return resp.data.content;
   } catch (error) {
